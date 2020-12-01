4ccbad3191cbe46918740656a64891bfc19e5acb
Pavel Ausianikau <lensli.p@gmail.com>
2020-12-01 17:27:37 +0300

    added value for slider (bugs is still)

--- viewColorChange/ViewController.swift
+++ viewColorChange/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var myView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    let startValueOfSlider: Float = 3
+    let minimumValueOfSlider: Float = 1
+    let maximumValueOfSlider: Float = 9
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Setup sliders
+        redSlider.value = startValueOfSlider
+        redSlider.minimumValue = minimumValueOfSlider
+        redSlider.maximumValue = maximumValueOfSlider
+        
+        greenSlider.value = startValueOfSlider
+        greenSlider.minimumValue = minimumValueOfSlider
+        greenSlider.maximumValue = maximumValueOfSlider
+        
+        blueSlider.value = startValueOfSlider
+        blueSlider.minimumValue = minimumValueOfSlider
+        blueSlider.maximumValue = maximumValueOfSlider
+        
+        // naming labels
+        redLabel.text = "Red:"
+        greenLabel.text = "Green"
+        blueLabel.text = "Blue"
     }
-
-
+    
+//    @IBAction func redSliderAction() {
+//        redLabel.text = "Red: \(String(redSlider.value))"
+//    }
+    
 }
 
